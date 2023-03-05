36f5f47395e6cfee103bbca2007591b81ebbd3ec
Sunday Akiyesi <akiyesis@gmail.com>
2023-03-05 14:17:47 -0500

    update repo

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -13,22 +13,22 @@ function createFeatures(earthquakeData) {
     // Give each feature a popup describing the place and time of the earthquakes
     function onEachFeature(feature, layer){
       layer.bindPopup("<h3> Where: " + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<br><h2> Magnitude: " + feature.properties.mag + "</h2>");
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<br><h2> Magnitude: " + feature.properties.mag +  "</h2>" + "<br><h2> depth: " + feature.geometry.coordinates[2] + "</h2>");
     }
 
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Create a variable for earthquakes to house latlng, each feature for popup, and cicrle radius/color/weight/opacity
     function createCircleMarker(feature, latlng){
        let options = {
         radius:feature.properties.mag*5,
-        fillColor: chooseColor(feature.properties.mag),
-        color: chooseColor(feature.properties.mag),
+        fillColor: chooseColor(feature.geometry.coordinates[2]),
+        color: "black",
         weight: 1,
         opacity: 0.8,
         fillOpacity: 0.35
        } 
        return L.circleMarker(latlng,options);
     }
-    // Create a variable for earthquakes to house latlng, each feature for popup, and cicrle radius/color/weight/opacity
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
     let earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature,
         pointToLayer: createCircleMarker
@@ -37,43 +37,35 @@ function createFeatures(earthquakeData) {
     // Send earthquakes layer to the createMap function - will start creating the map and add features
     createMap(earthquakes);
 }
+// Function to determine marker size
+function markerSize(magnitude) {
+  return magnitude * 10000;
+};
 
-// Circles color palette based on mag (feature) data marker: data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
-function chooseColor(mag){
-    switch(true){
-        case(1.0 <= mag && mag <= 2.5):
-            return "#0071BC"; // Strong blue
-        case (2.5 <= mag && mag <=4.0):
-            return "#35BC00";
-        case (4.0 <= mag && mag <=5.5):
-            return "#BCBC00";
-        case (5.5 <= mag && mag <= 8.0):
-            return "#BC3500";
-        case (8.0 <= mag && mag <=20.0):
-            return "#BC0000";
-        default:
-            return "#E2FFAE";
-    }
+// Circles color palette based on depth (feature) data marker: data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
+function chooseColor(depth) {
+  if (depth > 90) return "#43A047";
+  else if (depth > 70) return "#64B5F6";  
+  else if (depth > 50) return "#FFF176";
+  else if (depth > 30) return "#FB8C00";
+  else if (depth > 10) return "#B71C1C";
+  else return "#FF3300";
 }
 
-// Create map legend to provide context for map data
+// Create map legend to provide context for map data 
 let legend = L.control({position: 'bottomright'});
 
-legend.onAdd = function() {
+legend.onAdd = function(myMap) {
     var div = L.DomUtil.create('div', 'info legend');
-    var grades = [1.0, 2.5, 4.0, 5.5, 8.0];
-    var labels = [];
-    var legendInfo = "<h4>Magnitude</h4>";
-    div.innerHTML = legendInfo
-    
+    div.innerHTML += "<h4 style='text-align: center'> Depth </h4>";
+         grades = [-10,10,30, 50,70,90]  
+         colors = ["#FF3300", "#B71C1C", "#FB8C00","#FFF176", "#64B5F6", "#43A047"]
 // loop through density intervals
-for (var i = 0; i < grades.length; i++) {
-  labels.push('<ul style="background-color:' + chooseColor(grades[i] + 1) + '"> <span>' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '' : '+') + '</span></ul>');
+for (let i = 0; i < grades.length; i++) {
+    div.innerHTML +=
+        '<i style="background:' + colors[i]  + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
 }
-
-// add each label list item to the div under the <ul> tag
-div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-
 return div;
 };
 
@@ -114,7 +106,7 @@ var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
   legend.addTo(myMap)
 
 }
-
